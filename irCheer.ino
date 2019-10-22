52b9e6e882b6b27191a25049e269da47e1bed098
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2019-10-22 06:41:34 -0400

    Create irCheer.ino

---
+++ irCheer.ino
@@ -0,0 +1,14 @@
+
+#include <IRremote.h>
+IRsend irsend;
+   int khz = 38; // 38kHz carrier frequency for the NEC protocol
+   String colorCode[] ={"F720DF","F710EF",  "F730CF",  "F708F7",  "F728D7",  "F7A05F",  "F7906F",  "F7B04F",  "F78877",  "F7A857",  "F7609F",  "F750AF",  
+    "F7708F",  "F748B7",  "F76897",  "F7E01F",    "F720DF",    "F7A05F",  "F7609F", }
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+for 
+}
